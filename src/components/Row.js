a5c909f30999bc19a96765d7caf1c2cbff1eea56
Carolina R <carolinarizk@gmail.com>
2021-03-12 21:19:16 -0500

    added initial/simple styles

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Row({ employee, id }) {
   return (
-    <tr>
-      <td>{employee.name.first}</td>
-      <td>{employee.name.last}</td>
-      <td>{employee.email}</td>
-      <td>
-        <img src={employee.picture.thumbnail} alt={employee.name.first} />
-      </td>
-    </tr>
+      <tr>
+        <td>{employee.name.first}</td>
+        <td>{employee.name.last}</td>
+        <td>{employee.email}</td>
+        <td>
+          <img src={employee.picture.thumbnail} alt={employee.name.first} />
+        </td>
+      </tr>
   );
 }
 export default Row;
